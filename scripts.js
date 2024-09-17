9aca28aa3a107f4b7aff7ab9da3317e5f0c0e71e
zarika-amber <zarika.amber@gmail.com>
2024-09-17 15:01:09 -0700

    adds scripts.js file w/ widget code for ko-fi

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// ko-fi widget
+kofiWidgetOverlay.draw('zarikaamber', {
+  'type': 'floating-chat',
+  'floating-chat.donateButton.text': 'Support me',
+  'floating-chat.donateButton.background-color': '#794bc4',
+  'floating-chat.donateButton.text-color': '#fff'
+});
